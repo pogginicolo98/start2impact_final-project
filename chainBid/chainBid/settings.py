511c52f36bdd373d3b33b27fe85966fa982144f6
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-09-14 09:32:11 +0200

    Setup project

--- chainBid/chainBid/settings.py
+++ chainBid/chainBid/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'auctions',
+    'core',
 ]
 
 MIDDLEWARE = [
