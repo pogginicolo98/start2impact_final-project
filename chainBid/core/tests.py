5b25fcca76b0a8db10b2c9a735705ff656548363
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-09-14 22:49:43 +0200

    Add base templates and automated tests

--- chainBid/core/tests.py
+++ chainBid/core/tests.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from users.models import CustomUser
 
-# Create your tests here.
+
+class IndexTemplateViewTestCase(TestCase):
+    """
+    IndexTemplateView test case.
+    """
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='testcase', password='Change_me_123!')
+        self.url = reverse('entry-point')
+
+    def test_entry_point_url_by_name_by_not_authenticated_user(self):
+        expected_url = reverse('login') + "?next=" + self.url
+        response = self.client.get(self.url)
+        self.assertRedirects(
+            response=response,
+            expected_url=expected_url,
+            status_code=status.HTTP_302_FOUND,
+            target_status_code=status.HTTP_200_OK
+        )
+
+    def test_entry_point_url_by_name_by_authenticated_user(self):
+        self.client.login(username='testcase', password='Change_me_123!')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        if settings.DEBUG:
+            self.assertTemplateUsed(response, 'index-dev.html')
+        else:
+            self.assertTemplateUsed(response, 'index.html')
--- chainBid/core/views.py
+++ chainBid/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class IndexTemplateView(LoginRequiredMixin, TemplateView):
+    """
+    Render the homepage template.
+
+    * Only authenticated users can access to the homepage.
+    """
+
+    def get_template_names(self):
+        if settings.DEBUG:
+            template_name = 'index-dev.html'
+        else:
+            template_name = 'index.html'
+        return template_name
