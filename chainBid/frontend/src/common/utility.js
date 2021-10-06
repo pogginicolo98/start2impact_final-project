274ae83fdfb440b50c573fbf10633b6bdd938222
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-10-06 11:35:52 +0200

    Add schedule auction page

---
+++ chainBid/frontend/src/common/utility.js
@@ -0,0 +1,16 @@
+function countDecimalPlaces(number) {
+  /*
+    Counts the decimal places of a given number.
+  */
+
+  let match = (''+number).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+  if (!match) { return 0; }
+  return Math.max(
+       0,
+       // Number of digits right of decimal point.
+       (match[1] ? match[1].length : 0)
+       // Adjust for scientific notation.
+       - (match[2] ? +match[2] : 0));
+};
+
+export { countDecimalPlaces };
