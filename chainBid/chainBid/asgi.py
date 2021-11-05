846185c7072f4d6553799855a83f067b602841bf
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-11-05 22:53:12 +0100

    Setup for production

--- chainBid/chainBid/asgi.py
+++ chainBid/chainBid/asgi.py
@@ -12,7 +12,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chainBid.settings')
 
