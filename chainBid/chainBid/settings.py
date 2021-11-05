92b4998a3119969cc2bb5dc529bcc57ce564538d
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-11-05 16:20:56 +0100

    Setup for production

--- chainBid/chainBid/settings.py
+++ chainBid/chainBid/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = password.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["192.168.1.134"]
 
 
 # Application definition
