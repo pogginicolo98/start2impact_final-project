d5dd2457632d2fa989e80f2197a6c80cacedd06e
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-11-06 00:33:10 +0100

    Setup for production

--- chainBid/chainBid/asgi.py
+++ chainBid/chainBid/asgi.py
@@ -17,11 +17,9 @@ import auctions.routing
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
 
 
 application = ProtocolTypeRouter({
-  # "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             auctions.routing.websocket_urlpatterns
