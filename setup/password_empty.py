0702e3d80804f650995f04b10d5d47f790f63f5b
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-11-09 11:48:40 +0100

    Add setup files

---
+++ setup/password_empty.py
@@ -0,0 +1,7 @@
+# settings.py
+SECRET_KEY = '<DJANGO_SECRET_KEY>'
+
+# wallet.py, transactions.py
+network_url = '<ETHEREUM_API_NETWORK_URL>'
+address = '<ETHEREUM_WALLET_ADDRESS>'
+private_key = '<ETHEREUM_WALLET_PRIVATE_KEY>'
